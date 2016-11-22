e99494bf30579ef4f5206c4de6c38521d13943b3
Max Bobbitt <maxbobbitt@gmail.com>
2016-11-22 11:14:28 +0100

    unfinished ecercises

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,8 +1,18 @@
 class CoachingController < ApplicationController
   def answer
-    print params
+    @question= params[:question]
+
+
+    if @question.downcase == "i am going to work right now!"
+      @answer = "That is great!"
+    elsif @question.end_with?("?")
+      @answer= "Silly question, get dressed and go to work!"
+    else
+      @answer= "I don't care, get dressed and go to work!"
+    end
   end
 
   def ask
+
   end
 end
